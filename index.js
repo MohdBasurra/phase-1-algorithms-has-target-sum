41a901a3d2fba11f7a080e6be1ba9a6e9a4fe624
MohdBasurra <mohdye96@gmail.com>
2023-03-19 15:49:00 +0300

    assigment completed

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 function hasTargetSum(array, target) {
   // Write your algorithm here
+  const seenNumbers = new Set();
+  for (let number of array) {
+    const difference = target - number;
+    if (seenNumbers.has(difference)) {
+      return true;
+    }
+    seenNumbers.add(number);
+  }
+  return false;
 }
 
-/* 
-  Write the Big O time complexity of your function here
-*/
-
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
+ 
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
@@ -30,5 +29,6 @@ if (require.main === module) {
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
 }
+
 
 module.exports = hasTargetSum;
